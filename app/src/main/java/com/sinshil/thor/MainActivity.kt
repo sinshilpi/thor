6e5ac439cbcfef4db68e9fbc34897c9d723bf385
sinshil <shilpi.singh@careem.com>
2019-03-25 12:41:55 +0400

    working navigation drawer

--- app/src/main/java/com/sinshil/thor/MainActivity.kt
+++ app/src/main/java/com/sinshil/thor/MainActivity.kt
@@ -11,16 +11,25 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.model.LatLng
 import android.graphics.Color
 import android.location.Location
+import android.support.design.widget.NavigationView
+import android.support.v4.view.GravityCompat
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.ActionBar
+import android.support.v7.widget.Toolbar
+import android.view.MenuItem
 import com.google.android.gms.maps.model.CircleOptions
 
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var googleMap: GoogleMap
+    private lateinit var drawerLayout: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupDrawerLayout()
+        showToolbar()
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
@@ -32,6 +41,43 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
         googleMap.uiSettings.isZoomControlsEnabled = true
         googleMap.setMinZoomPreference(11f)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+                    drawerLayout.closeDrawer(GravityCompat.START)
+                } else {
+                    drawerLayout.openDrawer(GravityCompat.START)
+                }
+
+                true
+            }
+            else ->  super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun showToolbar() {
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val actionBar: ActionBar? = supportActionBar
+        actionBar?.apply {
+            setDisplayHomeAsUpEnabled(true)
+            setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp)
+        }
+    }
+
+    private fun setupDrawerLayout() {
+        drawerLayout = findViewById(R.id.drawer_container)
+
+        val navigationView: NavigationView = findViewById(R.id.navigation_view)
+        navigationView.setNavigationItemSelectedListener { menuItem ->
+            menuItem.isChecked = true
+            drawerLayout.closeDrawers()
+            true
+        }
     }
 
     private fun enableMyLocationsIfPermitted() {
